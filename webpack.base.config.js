177c24835dd4dbe64998fa79bc407c3d1b4bfeaa
Mr_WC <wanghaoran0218@hotmail.com>
2016-10-10 09:42:41 +0800

    init

---
+++ webpack.base.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var _ = require('lodash');
+var autoprefixer = require('autoprefixer');
+
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports={
+    entry: path.resolve(__dirname,"src/main.js")
+}
